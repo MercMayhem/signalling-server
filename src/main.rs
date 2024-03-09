ca34feb9bf0c233acb0b82ed5e332f565fcf7d56
MercMayhem <amanrao032@gmail.com>
2024-03-09 20:56:18 +0530

    temp save

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-
+use uuid::Uuid;
 use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response, request::RequestContext};
 use aws_sdk_dynamodb::{Client, types::AttributeValue};
 use aws_config::from_env;
@@ -14,16 +14,18 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let config = from_env().region("ap-south-1").load().await;
     let client = Client::new(&config);
 
-    println!("The Config: {:?}", config);
-
     if let RequestContext::WebSocket(request_context) = event.request_context(){
         let route = request_context.route_key;
-        let connection_id = request_context.connection_id;
+        let connection_id = request_context.connection_id.unwrap();
         
         match route {
             Some(route) => {
                 if route == "$connect" {
-                    handle_connect(&client, &connection_id.unwrap()).await?;
+                    handle_connect(&client, &connection_id).await?;
+                }
+
+                if route == "create" {
+                    handle_create(&client, &connection_id).await?;
                 }
             },
 
@@ -41,10 +43,35 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
 async fn handle_connect(client: &Client, connection_id: &str) -> Result<(), Error>{
     let table = env::var("CONN_TABLE")?;
     let pkey = env::var("CONN_PKEY")?;
-
+ 
     client.put_item()
         .table_name(table)
         .item(pkey, AttributeValue::S(connection_id.to_string()))
+        .send().await?;
+
+    return Ok(())
+}
+
+async fn handle_create(client: &Client, connection_id: &str) -> Result<(), Error>{
+    let connection_table = env::var("CONN_TABLE")?;
+    let room_table = env::var("ROOM_TABLE")?;
+    let connection_pkey = env::var("CONN_PKEY")?;
+    let room_pkey = env::var("ROOM_PKEY")?;
+
+    let uuid = Uuid::new_v4();
+
+    client.update_item()
+        .table_name(connection_table)
+        .key(connection_pkey, AttributeValue::S(connection_id.to_string()))
+        .update_expression("SET RoomID = :uuid")
+        .expression_attribute_values(":uuid", AttributeValue::S(uuid.to_string()))
+        .send().await?;
+    
+    client.put_item()
+        .table_name(room_table)
+        .item(room_pkey, AttributeValue::S(uuid.to_string()))
+        .item("created_by", AttributeValue::S(connection_id.to_string()))
+        .item("subscribers", AttributeValue::Ss(vec![connection_id.to_string()]))
         .send().await?;
 
     return Ok(())
